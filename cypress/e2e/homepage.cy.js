3cd074a859fb4b3b7d32edbb19d2efbe174bdeea
TeoKac <32864453+TeoKac@users.noreply.github.com>
2023-02-24 11:15:32 +0100

    changes for homepage.cy.js

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,16 +1,14 @@
 describe('Check if page returns 200', () => {
   beforeEach(() => {
-      cy.visit('https://app.sea.stage.veremark.com')
+    cy.visit('/login')
   })
 
   it('Only checks if the page returns code 200', () => {
-      cy.request({
-          url: 'https://app.sea.stage.veremark.com',
-        }).then((resp) => {
-          expect(resp.status).to.eq(200)
-        })
-        cy.contains("Forgot password?").should("be.visible")
-        cy.log("The page is working and returning code 200")
+    cy.request({
+      url: '/login'
+    }).then(resp => {
+      expect(resp.status).to.eq(200)
+    })
+    cy.log('The page is working and returning code 200')
   })
 })
-  
